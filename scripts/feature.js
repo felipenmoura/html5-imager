4f41d01f2089dc485ccca6b4f75dd6c317c3f389
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2013-02-05 00:13:58 -0200

    Adding better comments to the source code

--- scripts/feature.js
+++ scripts/feature.js
@@ -18,108 +18,166 @@ $(document).ready(function(){
             curPixel: null
         };
     
-    $('#hue').slider({
-        value: 50,
-        max: 100,
-        min: 0,
-        step: 1,
-        animate: false,
-        slide: function(evt, handle){
-            _conf.h= handle.value/100;
-            _applyEffect();
-            _getPixel(_conf.coords);
-        }
-    });
-    $('#saturation').slider({
-        value: 50,
-        max: 100,
-        min: 0,
-        step: 1,
-        animate: false,
-        slide: function(evt, handle){
-            _conf.s= handle.value/100;
-            _applyEffect();
-            _getPixel(_conf.coords);
-        }
-    });
-    $('#light').slider({
-        value: 50,
-        max: 100,
-        min: 0,
-        step: 1,
-        animate: false,
-        slide: function(evt, handle){
-            _conf.l= handle.value/100;
-            _applyEffect();
-            _getPixel(_conf.coords);
-        }
-    });
-    
-    $('#thumb-precision').slider({
-        value: 8,
-        max: 24,
-        min: 6,
-        step: 2,
-        range: 'min',
-        orientation: 'vertical',
-        slide: function(evt, handle){
-            _conf.precision= handle.value;
-            _getPixel(_conf.coords);
-        }
-    });
-    
-    $('#filter-level').slider({
-        value: 1,
-        max: 254,
-        min: 1,
-        step: 1,
-        range: 'min',
-        slide: function(evt, handle){
-            _conf.filterLevel= handle.value;
-            _applyEffect();
-        }
-    });
-    $('#applyFilter').click(function(){
-        if(this.checked)
-            _conf.applyFilter= true;
-        else
-            _conf.applyFilter= false;
-        _applyEffect();
-    });
-    
-    $('#newImage-ipt').change(function(evt){
-        var f = evt.target.files[0];
-        var fr = new FileReader();
+    var _constructor= function(){
+        
+        /**
+         * Apply the slider widget to Hue.
+         * 
+         * This also sets its method to be executed when sliding.
+         * In this case, it changes the hue of the openes image.
+         */
+        $('#hue').slider({
+            value: 50,
+            max: 100,
+            min: 0,
+            step: 1,
+            animate: false,
+            slide: function(evt, handle){
+                _conf.h= handle.value/100;
+                _applyEffect();
+                _getPixel(_conf.coords);
+            }
+        });
 
-        fr.onload = function(ev2) {
-            $('#images-list').append('<img src="'+ev2.target.result+'" class="thumb"/>');
-        };
+        /**
+         * Apply the slider widget to saturation.
+         * 
+         * This also sets its method to be executed when sliding.
+         * In this case, it changes the saturation of the openes image.
+         */
+        $('#saturation').slider({
+            value: 50,
+            max: 100,
+            min: 0,
+            step: 1,
+            animate: false,
+            slide: function(evt, handle){
+                _conf.s= handle.value/100;
+                _applyEffect();
+                _getPixel(_conf.coords);
+            }
+        });
 
-        fr.readAsDataURL(f);
-    });
-    
-    $('#download-btn').click(function(){
-        window.open(_canvas.toDataURL());
-    });
-    
-    $('#images-list').click(function(evt){
-        if(evt.target.tagName == 'IMG'){
-            var img= new Image();
-            __canvas.addClass('loading');
-            img.onload= function(){
-                _canvas.width= this.width;
-                _canvas.height= this.height;
-                _ctx.drawImage(img, 0, 0, this.width, this.height);
-                __canvas.removeClass('loading');
-                __canvas.addClass('withBG');
-                _canvasImgData= _ctx.getImageData(0, 0, this.width, this.height);
-                _originalImgData= _ctx.getImageData(0, 0, this.width, this.height);
+        /**
+         * Apply the slider widget to Light.
+         * 
+         * This also sets its method to be executed when sliding.
+         * In this case, it changes the light of the openes image.
+         * 
+         * TODO: it does now work as expected! :/
+         */
+        $('#light').slider({
+            value: 50,
+            max: 100,
+            min: 0,
+            step: 1,
+            animate: false,
+            slide: function(evt, handle){
+                _conf.l= handle.value/100;
+                _applyEffect();
+                _getPixel(_conf.coords);
+            }
+        });
+
+        /**
+         * Apply the slider widget to precision.
+         * 
+         * Precision changes the number of pixels to be visualized in the left
+         * panel once a pixel is selected.
+         */
+        $('#thumb-precision').slider({
+            value: 8,
+            max: 24,
+            min: 6,
+            step: 2,
+            range: 'min',
+            orientation: 'vertical',
+            slide: function(evt, handle){
+                _conf.precision= handle.value;
+                _getPixel(_conf.coords);
+            }
+        });
+
+        /**
+         * Apply the slider widget to filter level.
+         * 
+         * The filter level is the sensitivity of the filter to remove all the
+         * pixels with a given color.
+         */
+        $('#filter-level').slider({
+            value: 1,
+            max: 254,
+            min: 1,
+            step: 1,
+            range: 'min',
+            slide: function(evt, handle){
+                _conf.filterLevel= handle.value;
+                _applyEffect();
+            }
+        });
+
+        /**
+         * Checks if it should or not apply the filter(removing pixels)
+         */
+        $('#applyFilter').click(function(){
+            if(this.checked)
+                _conf.applyFilter= true;
+            else
+                _conf.applyFilter= false;
+            _applyEffect();
+        });
+
+        /**
+         * The file input handler.
+         */
+        $('#newImage-ipt').change(function(evt){
+            var f = evt.target.files[0];
+            var fr = new FileReader();
+
+            fr.onload = function(ev2) {
+                $('#images-list').append('<img src="'+ev2.target.result+'" class="thumb"/>');
             };
-            img.src= evt.target.src;
-        }
-    });
+
+            fr.readAsDataURL(f);
+        });
+
+        /**
+         * The download button's action.
+         */
+        $('#download-btn').click(function(){
+            window.open(_canvas.toDataURL());
+        });
+
+        /**
+         * Adds the actions for the thumbnails on the list of images.
+         */
+        $('#images-list').click(function(evt){
+            if(evt.target.tagName == 'IMG'){
+                var img= new Image();
+                __canvas.addClass('loading');
+                img.onload= function(){
+                    _canvas.width= this.width;
+                    _canvas.height= this.height;
+                    _ctx.drawImage(img, 0, 0, this.width, this.height);
+                    __canvas.removeClass('loading');
+                    __canvas.addClass('withBG');
+                    _canvasImgData= _ctx.getImageData(0, 0, this.width, this.height);
+                    _originalImgData= _ctx.getImageData(0, 0, this.width, this.height);
+                };
+                img.src= evt.target.src;
+            }
+        });
+        
+        // when clicked, the canvas should get the current pixel.
+        __canvas.click(_getPixel);
+
+        _canvas.width= document.body.clientWidth - 255;
+        _canvas.height= document.body.clientHeight - 94;
+    }
     
-    
+    /**
+     * Removes all the applied effects.
+     */
     var _resetEffects= function(){
         
         _conf= {
@@ -135,9 +193,9 @@ $(document).ready(function(){
         
         _ctx.putImageData(_originalImgData, 0, 0);
         _canvasImgData= _ctx.getImageData(0, 0, _canvas.width, _canvas.height);
-        //_applyEffect();
     };
     
+    // adding the reset method to the respective button.
     $('#clear-btn').click(_resetEffects);
     
     /**
@@ -211,7 +269,11 @@ $(document).ready(function(){
     }
     
     
-    
+    /**
+     * Applies the defined effects.
+     * 
+     * It uses the current hsl and filter settings to apply them to the image.
+     */
     var _applyEffect= function(){
         
         var r, g, b, i=0, hsl, rgb, alpha,
@@ -260,6 +322,12 @@ $(document).ready(function(){
         _ctx.putImageData(frame, 0, 0);
     };
     
+    /**
+     * Retrieves a pixel.
+     * 
+     * It receives a click event, or an array with the coordinates of the click.
+     * This method sets the current focused pixel as clicked.
+     */
     var _getPixel= function(evt){
         
         if(!evt)
@@ -320,8 +388,5 @@ $(document).ready(function(){
         
     };
     
-    __canvas.click(_getPixel);
-    
-    _canvas.width= document.body.clientWidth - 255;
-    _canvas.height= document.body.clientHeight - 94;
+    _constructor();
 });
